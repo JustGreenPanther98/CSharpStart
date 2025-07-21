eda212b0156effc04eaa3a4a6a7938cd7b7a45af
vedan <vedan@DESKTOP-PM3BL68>
2025-07-22 00:36:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿String s="why";
+s.Replace("y", "sh"); 
+String[] w = s.Split("");
+s=s.Insert(1, "d");
+Console.WriteLine(s.Reverse());
+for(int i = 0; i < s.Length; i++)
+    Console.WriteLine(s[i]);
---
+++ TicTacToe.cs
@@ -0,0 +1,29 @@
+﻿public class TicTacToe
+{
+    static void main(String[] args)
+    {
+        string[,] TTT = { {"1","2","3" },{"4","5","6"},{"7","8","9" } };
+        int counter = 0;//for constructor use nothing else
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                TTT[i,j] = Convert.ToString(counter);
+                counter++;
+            }
+        }
+        display(TTT);
+    }
+    public static void display(String[,]arr)
+    {
+        for(int i = 0;i < 3;i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(arr[i,j]);
+            }
+            if(i!=2) 
+                Console.WriteLine("------");
+        }
+    }
+}
